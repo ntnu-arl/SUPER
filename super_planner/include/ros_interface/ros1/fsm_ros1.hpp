e0f5ea111f85823737c87f8b25f3462668de2c28
Mihir Dharmadhikari <mihir.dharmadhikari@ntnu.no>
2025-09-20 15:20:17 +0200

    Handling empty trajectory

--- super_planner/include/ros_interface/ros1/fsm_ros1.hpp
+++ super_planner/include/ros_interface/ros1/fsm_ros1.hpp
@@ -82,7 +82,11 @@ namespace fsm {
                 trajectory_msgs::MultiDOFJointTrajectory traj_msg;
                 bool exec = true;
                 getCommittedMultidofTrajectory(traj_msg, exec);
-                if(exec)
+                if(traj_msg.points.size() <= 0)
+                {
+                    cout << RED << "[Fsm] Committed trajectory empty!!" << RESET << endl;
+                }
+                if(exec && traj_msg.points.size() > 0)
                     multi_dof_traj_pub_.publish(traj_msg);
             }
         }
